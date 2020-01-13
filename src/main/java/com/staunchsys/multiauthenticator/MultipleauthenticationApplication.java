808cef1fa51e8e151514721faa85905f987d3daf
jdmaheta <jmaheta@staunchsys.com>
2020-01-13 17:20:12 +0530

    Initial check in

---
+++ src/main/java/com/staunchsys/multiauthenticator/MultipleauthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.staunchsys.multiauthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleauthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleauthenticationApplication.class, args);
+	}
+
+}
